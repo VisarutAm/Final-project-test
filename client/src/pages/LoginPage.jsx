ce806863a58238a1ff2c47cd59d20119eb9646a8
James <apisitamornktn@gmail.com>
2024-07-09 01:12:53 +0700

    feat:add validate data

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,28 +2,67 @@ import { useState } from "react";
 import { useAuth } from "../contexts/authentication";
 import FacebookIcon from "../assets/icons/facebook-icon.svg";
 import { useNavigate } from "react-router-dom";
+import ExclamationIcon from "../assets/icons/exclamation-icon.svg";
 
 function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
 
+  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
-  const { login } = useAuth();
+  const { login, state } = useAuth();
+
+  const validateFields = () => {
+    let newErrors = {};
+
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!email || !emailPattern.test(email))
+      newErrors.email = "กรุณากรอกอีเมลให้ถูกต้อง";
+
+    if (!password) newErrors.password = "กรุณากรอกรหัสผ่าน";
+    setErrors(newErrors);
+    return newErrors;
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    try {
+    const newErrors = validateFields();
+    if (Object.keys(newErrors).length === 0) {
       await login({ email, password });
-      navigate("/");
-    } catch (err) {
-      setError("Invalid email or password");
     }
+  };
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    let newErrors = { ...errors };
+
+    switch (id) {
+      case "email":
+        const emailPattern =
+          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        setEmail(value);
+        if (value && emailPattern.test(value)) {
+          delete newErrors.email;
+        } else {
+          newErrors.email = "กรุณากรอกอีเมลให้ถูกต้อง";
+        }
+        break;
+      case "password":
+        setPassword(value);
+        if (value) {
+          delete newErrors.password;
+        } else {
+          newErrors.password = "กรุณากรอกรหัสผ่าน";
+        }
+        break;
+      default:
+        break;
+    }
+    setErrors(newErrors);
   };
 
   return (
     <>
-      {/* รอแก้ เพิ่ม navbar */}
       <div className="min-h-screen flex items-center justify-center bg-gray-100">
         <div className="w-full max-w-md p-8 bg-white rounded-lg border border-gray-300 shadow-md">
           <h2 className="text-2xl font-medium mb-6 text-center text-blue-900">
@@ -36,16 +75,30 @@ function LoginPage() {
                 className="block text-sm font-medium text-gray-700"
               >
                 อีเมล
+                {errors.email && <span className="text-red-500">*</span>}
               </label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                placeholder="กรุณากรอกอีเมล"
-                required
-              />
+              <div className="relative">
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={handleChange}
+                  className={`mt-1 block w-full px-3 py-2 border ${
+                    errors.email ? "border-red-500" : "border-gray-300"
+                  } rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+                  placeholder="กรุณากรอกอีเมล"
+                />
+                {errors.email && (
+                  <img
+                    src={ExclamationIcon}
+                    alt="error"
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2"
+                  />
+                )}
+              </div>
+              {errors.email && (
+                <p className="text-red-500 text-xs mt-1">{errors.email}</p>
+              )}
             </div>
             <div className="mb-4">
               <label
@@ -53,22 +106,44 @@ function LoginPage() {
                 className="block text-sm font-medium text-gray-700"
               >
                 รหัสผ่าน
+                {errors.password && <span className="text-red-500">*</span>}
               </label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                placeholder="กรุณากรอกรหัสผ่าน"
-                required
-              />
+              <div className="relative">
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={handleChange}
+                  className={`mt-1 block w-full px-3 py-2 border ${
+                    errors.password ? "border-red-500" : "border-gray-300"
+                  } rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+                  placeholder="กรุณากรอกรหัสผ่าน"
+                />
+                {errors.password && (
+                  <img
+                    src={ExclamationIcon}
+                    alt="error"
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2"
+                  />
+                )}
+              </div>
+              {errors.password && (
+                <p className="text-red-500 text-xs mt-1">{errors.password}</p>
+              )}
             </div>
-            {error && <div className="mb-4 text-red-600">{error}</div>}
+            {state.error && (
+              <div className="mb-4 text-red-600">
+                {state.error === "User not found"
+                  ? "ไม่พบผู้ใช้งานในระบบในระบบ"
+                  : state.error === "Invalid password"
+                  ? "รหัสผ่านไม่ถูกต้อง"
+                  : state.error}
+              </div>
+            )}
             <div className="mb-4">
               <button
                 type="submit"
-                className="w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                className="w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-500 focus:outline-none focus:hover:bg-blue-800"
               >
                 เข้าสู่ระบบ
               </button>
@@ -81,7 +156,7 @@ function LoginPage() {
               >
                 <img
                   src={FacebookIcon}
-                  className="w-5 h-5 mr-2"
+                  className="w-5 h-5 mr-2 text-blue-600"
                   alt="Facebook Icon"
                 />
                 เข้าสู่ระบบด้วย Facebook
--- server/routes/auth.mjs
+++ server/routes/auth.mjs
@@ -5,17 +5,71 @@ import supabase from "../utils/db.mjs";
 
 const authRouter = Router();
 
+const getRandomBackgroundColor = () => {
+  const colors = [
+    "007bff", // Blue
+    "28a745", // Green
+    "dc3545", // Red
+    "ffc107", // Yellow
+    "17a2b8", // Teal
+    "6610f2", // Purple
+    "fd7e14", // Orange
+    "20c997", // Cyan
+    "e83e8c", // Pink
+    "6f42c1", // Indigo
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const generateAvatarUrl = (firstname, lastname) => {
+  const initials = `${firstname[0]}${lastname[0]}`;
+  const backgroundColor = getRandomBackgroundColor();
+  return `https://ui-avatars.com/api/?name=${encodeURIComponent(
+    initials
+  )}&background=${backgroundColor}&color=ffffff`;
+};
+
+const validateFields = (firstname, lastname, tel_num, email, password) => {
+  let errors = {};
+
+  if (!firstname) errors.firstname = "กรุณากรอกชื่อ";
+  if (!lastname) errors.lastname = "กรุณากรอกนามสกุล";
+
+  const telPattern = /^[0-9]{10}$/;
+  if (!tel_num || !telPattern.test(tel_num))
+    errors.tel_num = "กรุณากรอกเบอร์โทรศัพท์ให้ถูกต้อง (10 หลัก)";
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!email || !emailPattern.test(email))
+    errors.email = "กรุณากรอกอีเมลให้ถูกต้อง";
+
+  if (!password) errors.password = "กรุณากรอกรหัสผ่าน";
+
+  return errors;
+};
+
 authRouter.post("/register", async (req, res) => {
   const { firstname, lastname, tel_num, email, password } = req.body;
 
+  const errors = validateFields(firstname, lastname, tel_num, email, password);
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json({ errors });
+  }
+
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
+    const profileImage = generateAvatarUrl(firstname, lastname);
 
-    const { error } = await supabase
-      .from("users")
-      .insert([
-        { firstname, lastname, tel_num, email, password: hashedPassword },
-      ]);
+    const { error } = await supabase.from("users").insert([
+      {
+        firstname,
+        lastname,
+        tel_num,
+        email,
+        password: hashedPassword,
+        profile_image: profileImage,
+      },
+    ]);
 
     if (error) {
       return res.status(400).json({ message: error.message });
@@ -33,13 +87,13 @@ authRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const { data: user, error } = await supabase
+    const { data: user } = await supabase
       .from("users")
       .select("*")
       .eq("email", email)
       .single();
 
-    if (error || !user) {
+    if (!user) {
       return res.status(404).json({
         message: "User not found",
       });
